0e7c88f57ac52dff0b9470f26ad5717aa42b9293
Pierre Chifflier <chifflier@wzdftpd.net>
2016-08-23 16:42:30 +0200

    Really raise exception on error

--- exception.c
+++ exception.c
@@ -5,11 +5,15 @@
 
 #include <exception.h>
 
+extern void raise_swig_error(const char *errstr);
+
 static char error_message[256];
 static int error_status = 0;
 
 void throw_exception(char *msg) {
         strncpy(error_message,msg,255);
+        error_message[255] = 0;
+        raise_swig_error(error_message);
         error_status = 1;
 }
 
